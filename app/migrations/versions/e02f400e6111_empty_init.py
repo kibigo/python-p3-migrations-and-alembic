6377e9e77ff92e1b3ead3433bcce581c201f5e15
kibigo <jacksonkibigo99@gmail.com>
2023-10-30 22:50:42 +0300

    Completed Assignment

---
+++ app/migrations/versions/e02f400e6111_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e02f400e6111
+Revises: 
+Create Date: 2023-10-30 22:46:33.502130
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e02f400e6111'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
